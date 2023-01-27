879307222f877dfe4dc7b5172083e3bb70d37c55
blck-snwmn <whitesnowmancreator@gmail.com>
2023-01-28 03:14:26 +0900

    Send slack

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
+
+export interface Env {
+  WEBHOOK_URL: string
+  FORWARD_EMAIL: string
+}
+
 export default {
-  async email(message, env, ctx) {
+  async email(message: EmailMessage, env: Env, ctx: ExecutionContext) {
+    const slack_resp = await fetch(env.WEBHOOK_URL, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(
+        {
+          response_type: "in_channel",
+          blocks: [
+            {
+              type: "header",
+              text: {
+                type: "plain_text",
+                text: message.from
+              }
+            }
+          ]
+        }),
+    })
     console.log("test")
     await message.forward(env.FORWARD_EMAIL);
   }
